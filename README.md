4ad89c1d0c4022584c1bb77bc80e5f4f977fd83c
xxbg <xxbgLC@163.com>
2018-08-09 17:05:19 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# 移动端页面测试
+
+## 背景
+> 移动端页面的开发常常需要考虑各种手机的兼容性，chrom浏览器的手机模拟并非真正手机上的实际显示.
+
+> 出于方便移动端调试，自己写了一个方便手机端调试的方法，分享给大家~
+
+## 原理
+
+> 基于nodejs的本地服务器，局域网访问
+
+## 使用
+
+1. 克隆代码到本地 
+```shell
+git clone https://github.com/xxbgLC/for_mobile_test.git
+```
+2. 启动服务
+```shell
+node server.js
+```
+3. 在手机端输入日志上的地址
+
+-----
+
+> tips: server.js 要与index.html同级（当然你也可以修改server.js里面的代码）
+
+
---
+++ server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const os = require('os')
+
+var server = http.createServer();
+var port = 4567
+var hostName = getLoaclIP()
+
+var handlerequest = function (request, respone) {
+  var url = request.url;
+  if (url == '/') {
+    respone.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile('./index.html', function (err, data) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      respone.end(data);
+    });
+  } else if (url != '/') {
+    var ourl = '.' + url;
+    var type = ourl.substr(ourl.lastIndexOf(".") + 1, ourl.length)
+    respone.writeHead(200, { 'Content-type': "text/" + type });
+    fs.readFile(ourl, function (err, data) {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      respone.end(data);
+    });
+  }
+}
+server.on('request', handlerequest);
+
+server.listen(port, hostName, function () {
+  console.log(`runing at http://${hostName}:${port}`);
+})
+
+// 获取本地ip
+function getLoaclIP() {
+  let ipObj = os.networkInterfaces();
+  let IPv4 = [];
+  Object.keys(ipObj).forEach(ele => {
+    ipObj[ele].forEach(ip => {
+      if (ip.family === 'IPv4') {
+        IPv4.push(ip.address);
+      }
+    })
+  });
+  return IPv4[0]
+}
